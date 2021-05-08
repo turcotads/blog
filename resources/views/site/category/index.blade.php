53739c5a7f419e141b5520b89666901d211e4c35
Fábio Madeira Peres <fabinhomadeira@gmail.com>
2021-05-08 11:01:42 -0300

    Adicionado os assets e htmls das páginas

--- resources/views/site/category/index.blade.php
+++ resources/views/site/category/index.blade.php
@@ -1,1 +1,26 @@
-aqui é a página de produtos
+@extends('layouts.site')
+
+@section('content')
+<header class="common-header">
+    <div class="header-wrapper">
+        <h1 class="title-large">With apples drink teriyaki. </h1>
+    </div>
+    <div class="pattern__header"></div>
+</header>
+
+<div class="gray-background">
+    <section class="products__cultivation__category main-wrapper">
+            <article class="card__product">
+                <a href="">
+                    <div class="card__cover">
+                        <img src="">
+                    </div>
+                    <header class="card__product-header">
+                        <h2 class="title-medium"></h2>
+                        <p></p>
+                    </header>
+                </a>
+            </article>
+    </section>
+</div>
+@endsection
